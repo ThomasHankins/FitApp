c18baa87b7c17901a336838dc379b57a2feab798
Thomas Hankins <hankinst@mcmaster.ca>
2022-07-04 22:51:13 -0400

    refactored to improve use of structures

--- lib/workout-tracker/data_structures/exercise_set.dart
+++ lib/workout-tracker/data_structures/exercise_set.dart
@@ -1,57 +1,132 @@
 import 'dart:async';
 
-abstract class LiveAction {
-  bool get isComplete;
-  set note(String note);
-  set restTime(int restTime);
-  String get note;
-  Map<String, dynamic> toMap();
-  void complete(int position, int parentPosition, int workoutID);
-  int get restTimeLeft;
-  set addTime(int time);
-  set removeTime(int time);
-  void startTimer();
-}
-
-abstract class HistoricAction {
-  int get position;
-}
-
-mixin ExerciseSet {
-  double get weight;
-  int get reps;
-  int get restTime;
-  int? get rpe;
-  String get note;
-}
-
-mixin Cardio {
-  int get duration;
-  int get distance;
-  int get restTime;
-  int? get calories;
-  String get note;
-}
-
-class LiveSet extends LiveAction with ExerciseSet {
-  @override
-  double weight;
-  @override
-  int reps;
-  @override
-  int restTime;
-  @override
-  int? rpe;
-  String _note = '';
-  late int _parentPosition;
+abstract class Action {
   late int _position;
+  int get position => _position;
+
+  int _restTime; //TODO refactor to make type duration
+  int get restTime => _restTime;
+
+  String _note;
+  String get note => _note;
+
+  Action({required restTime, required note})
+      : _restTime = restTime,
+        _note = note;
+}
+
+abstract class LiveAction extends Action {
+  late int _parentPosition;
   late int _workoutID;
-  bool _complete = false;
+
   //temp variables for the timer
   Timer? _countdownTimer;
   Duration _timerDurationRemaining = const Duration(seconds: 0);
 
-  LiveSet({required this.weight, required this.reps, required this.restTime});
+  bool _complete = false;
+  bool get isComplete => _complete;
+
+  void complete(int position, int parentPosition, int workoutID) {
+    _complete = true;
+    _position = position;
+    _parentPosition = parentPosition;
+    _workoutID = workoutID;
+    _countdownTimer?.cancel();
+  }
+
+  Map<String, dynamic> toMap();
+
+  void startTimer() {
+    if (_timerDurationRemaining.inSeconds == 0) {
+      _timerDurationRemaining = Duration(seconds: restTime);
+
+      _countdownTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
+        if (_timerDurationRemaining <= const Duration(seconds: 0)) {
+          _countdownTimer!.cancel();
+        } else {
+          _timerDurationRemaining -= const Duration(seconds: 1);
+        }
+      });
+    }
+  }
+
+  int get restTimeLeft => _timerDurationRemaining.inSeconds;
+
+  set restTime(int time) {
+    if (!isComplete) {
+      _restTime = time;
+      _timerDurationRemaining =
+          Duration(seconds: _timerDurationRemaining.inSeconds + time);
+    }
+  }
+
+  set addTime(int time) {
+    _restTime += time;
+    _timerDurationRemaining =
+        Duration(seconds: _timerDurationRemaining.inSeconds + time);
+  }
+
+  set removeTime(int time) {
+    addTime = -time;
+  }
+
+  set note(String note) =>
+      note.length < 30 ? _note = note : _note = note.substring(0, 30);
+
+  LiveAction({
+    required restTime,
+    required note,
+  }) : super(
+          restTime: restTime,
+          note: note,
+        );
+}
+
+abstract class HistoricAction extends Action {
+  HistoricAction({
+    required position,
+    required restTime,
+    required note,
+  }) : super(
+          restTime: restTime,
+          note: note,
+        ) {
+    _position = position;
+  }
+}
+
+mixin Strength on Action {
+  late double _weight;
+  double get weight => _weight;
+
+  late int _reps;
+  int get reps => _reps;
+
+  int? _rpe;
+  int? get rpe => _rpe;
+}
+
+mixin Cardio on Action {
+  late int _duration;
+  int get duration => _duration;
+
+  late int _distance;
+  int get distance => _distance;
+
+  int? _calories;
+  int? get calories => _calories;
+}
+
+class LiveSet extends LiveAction with Strength {
+  LiveSet({required double weight, required int reps, required int restTime})
+      : super(restTime: restTime, note: "") {
+    _weight = weight;
+    _reps = reps;
+  }
+
+  set weight(double weight) => _weight = weight;
+  set reps(int reps) => _reps = reps;
+  set rpe(int? rpe) => (rpe! >= 0 || rpe <= 10) ? _rpe = rpe : null;
 
   @override
   Map<String, dynamic> toMap() {
@@ -68,75 +143,22 @@ class LiveSet extends LiveAction with ExerciseSet {
   }
 
   @override
-  bool get isComplete => _complete;
-
-  @override
-  String get note => _note;
-  @override
-  set note(String note) =>
-      note.length < 30 ? _note = note : _note = note.substring(0, 30);
-
-  @override
   void complete(int position, int parentPosition, int workoutID) {
     if (reps > 0) {
-      _complete = true;
-      _position = position;
-      _parentPosition = parentPosition;
-      _workoutID = workoutID;
+      super.complete(position, parentPosition, workoutID);
+    } else {
+      super._countdownTimer?.cancel();
     }
-    _countdownTimer?.cancel();
-  }
-
-  @override
-  void startTimer() {
-    if (_timerDurationRemaining.inSeconds == 0) {
-      _timerDurationRemaining = Duration(seconds: restTime);
-
-      _countdownTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
-        if (_timerDurationRemaining <= const Duration(seconds: 0)) {
-          _countdownTimer!.cancel();
-        } else {
-          _timerDurationRemaining -= const Duration(seconds: 1);
-        }
-      });
-    }
-  }
-
-  @override
-  int get restTimeLeft => _timerDurationRemaining.inSeconds;
-
-  @override
-  set addTime(int time) {
-    restTime += time;
-    _timerDurationRemaining =
-        Duration(seconds: _timerDurationRemaining.inSeconds + time);
-  }
-
-  @override
-  set removeTime(int time) {
-    restTime -= time;
-    _timerDurationRemaining =
-        Duration(seconds: _timerDurationRemaining.inSeconds - time);
   }
 }
 
 class LiveCardio extends LiveAction with Cardio {
-  @override
-  int duration;
-  @override
-  int distance;
-  @override
-  int restTime;
-  @override
-  int? calories;
-  String _note = '';
-  late int _parentPosition;
-  late int _position;
-  late int _workoutID;
-  bool _complete = false;
-
   LiveCardio(
-      {required this.duration, required this.distance, required this.restTime});
+      {required int distance, required int duration, required int restTime})
+      : super(restTime: restTime, note: "") {
+    _distance = distance;
+    _duration = duration;
+  }
 
   @override
   Map<String, dynamic> toMap() {
@@ -153,15 +175,6 @@ class LiveCardio extends LiveAction with Cardio {
   }
 
   @override
-  bool get isComplete => _complete;
-
-  @override
-  String get note => _note;
-  @override
-  set note(String note) =>
-      note.length < 30 ? _note = note : _note = note.substring(0, 30);
-
-  @override
   void complete(int position, int parentPosition, int workoutID) {
     if (distance > 0 && duration > 0) {
       _complete = true;
@@ -170,35 +183,9 @@ class LiveCardio extends LiveAction with Cardio {
       _workoutID = workoutID;
     }
   }
-
-  @override
-  // TODO: implement restTimeLeft
-  int get restTimeLeft => throw UnimplementedError();
-
-  @override
-  set addTime(int time) {
-    // TODO: implement addTime
-  }
-
-  @override
-  set removeTime(int time) {
-    // TODO: implement removeTime
-  }
-
-  @override
-  void startTimer() {
-    // TODO: implement startTimer
-  }
 }
 
-class HistoricSet extends HistoricAction with ExerciseSet {
-  final int _position;
-  final double _weight;
-  final int _reps;
-  final int _restTime;
-  final int? _rpe;
-  final String _note;
-
+class HistoricSet extends HistoricAction with Strength {
   HistoricSet({
     required int position,
     required double weight,
@@ -206,40 +193,18 @@ class HistoricSet extends HistoricAction with ExerciseSet {
     required int restTime,
     required int? rpe,
     required String note,
-  })  : _position = position,
-        _weight = weight,
-        _reps = reps,
-        _restTime = restTime,
-        _rpe = rpe,
-        _note = note;
-
-  @override
-  int get position => _position;
-
-  @override
-  double get weight => _weight;
-
-  @override
-  int get reps => _reps;
-
-  @override
-  int get restTime => _restTime;
-
-  @override
-  int? get rpe => _rpe;
-
-  @override
-  String get note => _note;
+  }) : super(
+          position: position,
+          note: note,
+          restTime: restTime,
+        ) {
+    _weight = weight;
+    _reps = reps;
+    _rpe = rpe;
+  }
 }
 
 class HistoricCardio extends HistoricAction with Cardio {
-  final int _position;
-  final int _duration;
-  final int _distance;
-  final int _restTime;
-  final int? _calories;
-  final String _note;
-
   HistoricCardio({
     required int position,
     required int duration,
@@ -247,28 +212,13 @@ class HistoricCardio extends HistoricAction with Cardio {
     required int restTime,
     required int? calories,
     required String note,
-  })  : _position = position,
-        _duration = duration,
-        _distance = distance,
-        _restTime = restTime,
-        _calories = calories,
-        _note = note;
-
-  @override
-  int get position => _position;
-
-  @override
-  int get duration => _duration;
-
-  @override
-  int get distance => _distance;
-
-  @override
-  int get restTime => _restTime;
-
-  @override
-  int? get calories => _calories;
-
-  @override
-  String get note => _note;
+  }) : super(
+          position: position,
+          note: note,
+          restTime: restTime,
+        ) {
+    _duration = duration;
+    _distance = distance;
+    _calories = calories;
+  }
 }
